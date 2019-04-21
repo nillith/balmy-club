29cc30c0bf3da9f0c46c49b1c6f61839c7cf1c3f
nillith <nillith@outlook.com>
2019-04-21 11:55:12 +0800

    test: allow empty environment variable values when testing

--- src/server/env/index.ts
+++ src/server/env/index.ts
@@ -2,7 +2,7 @@ import dotEnvSafe from 'dotenv-safe';
 import path from 'path';
 
 dotEnvSafe.config({
-  allowEmptyValues: false,
+  allowEmptyValues: 'true' === process.env.ALLOW_EMPTY_ENV_VALUES,
   example: path.join(__dirname, '/example.env'),
   path: path.join(__dirname, '/.env')
 });
--- src/server/test-init.spec.ts
+++ src/server/test-init.spec.ts
@@ -1,3 +1,4 @@
+process.env.ALLOW_EMPTY_ENV_VALUES = 'true';
 import './init';
 
 
