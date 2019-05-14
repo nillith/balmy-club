34d279bdd1d9012ad7da5b0d6e7808949afb60ad
nillith <nillith@outlook.com>
2019-05-14 12:45:39 +0800

    fix: generate build.ts before build

--- build-scripts/update-version.js
+++ build-scripts/update-version.js
@@ -1,13 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const pkg = require('../package.json');
-const {getCommitCount, getUpdatedVersion, getBuildTsContent} = require('./utils');
-const ROOT = path.normalize(`${__dirname}/..`);
-const PACKAGE_PATH = `${ROOT}/package.json`;
+const fs = require('fs')
+const path = require('path')
+const pkg = require('../package.json')
+const {getCommitCount, getUpdatedVersion, getBuildTsContent} = require('./utils')
+const ROOT = path.normalize(`${__dirname}/..`)
+const PACKAGE_PATH = `${ROOT}/package.json`
 const BUILD_TS = `${ROOT}/src/shared/build.ts`;
+
 (async function () {
-  const commitCount = await getCommitCount();
-  pkg.version = await getUpdatedVersion(commitCount + 1);
-  fs.writeFileSync(PACKAGE_PATH, JSON.stringify(pkg, null, 2), 'utf8');
+  const commitCount = await getCommitCount()
+  const patch = 'precommit' === process.argv[2] ? commitCount + 1 : commitCount
+  pkg.version = await getUpdatedVersion(patch)
+  fs.writeFileSync(PACKAGE_PATH, JSON.stringify(pkg, null, 2), 'utf8')
   fs.writeFileSync(BUILD_TS, getBuildTsContent(pkg.version), 'utf8')
-})();
+})()
